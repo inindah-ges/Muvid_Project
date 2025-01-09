5e5f4b7cf0ae3742e30b75b50d0a85cdb2bf28c6
Ikramullah Latif <ikramullahzx@gmail.com>
2025-01-10 06:28:18 +0700

    fix(provider): pake app facades untuk akses APP_ENV

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,10 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
-
-        if (config('APP.ENV') === 'production') {
+        // Force HTTPS is environment is not Local
+        if (!App::environment('local')) {
             \URL::forceScheme('https');
         }
+
+        Paginator::useBootstrapFive();
     }
 }
