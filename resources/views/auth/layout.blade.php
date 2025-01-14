a42ec44fa666601531b294810034ebe58d5bd87c
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-01-14 09:01:21 +0700

    fix : lagi

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -42,10 +42,8 @@
 
     <!-- Sweet Alert -->
     <link type="text/css" href="{{ asset('auth/vendor/sweetalert2/dist/sweetalert2.min.css') }}" rel="stylesheet">
-
     <!-- Notyf -->
     <link type="text/css" href="{{ asset('auth/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
-
     <!-- Volt CSS -->
     <link type="text/css" href="{{ asset('auth/css/volt.css') }}" rel="stylesheet">
 
